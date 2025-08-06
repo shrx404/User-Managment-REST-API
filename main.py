f1ba8e9952d847592b828e7e831701d1a6d334af
Shreyas <4975shreyasy@gmail.com>
2025-08-06 19:17:59 +0530

    added the home function

--- main.py
+++ main.py
@@ -2,5 +2,14 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+@app.route("/")
+def he():
+    return "Homes"
+
+# GET - USED TO GET DATA
+# POST - USED TO PUT DATA
+# PUT - USED TO MODIFY THE ALREADY PRESENT DATA
+# DELETE - USED TO DELETE DATA
+
 if __name__ == "__main__" :
     app.run()
